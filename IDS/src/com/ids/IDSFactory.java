e37a76055e49126b6bb1f9170d8f841a68b40184
foxcats <ip9894@naver.com>
2017-04-28 02:33:48 +0900

    Update IDSFactory.java

--- IDS/src/com/ids/IDSFactory.java
+++ IDS/src/com/ids/IDSFactory.java
@@ -12,21 +12,23 @@ public class IDSFactory {
 	
 	public IDSearcher getLPS()
 	{
-		Matcher linkPolicyMatcher=new LinkPolicyMatcher();
-		Constructor candidateConstructor=new CandidateConstructor();
-		ObjectComparison objectComparison=new ObjectComparison();
+		Matcher linkPolicyMatcher=new LinkPolicyMatcher();//matching Link-policy btween sourceLOD and targetLOD
+		Constructor candidateConstructor=new CandidateConstructor();//make candidate from targetLOD
+		ObjectComparison objectComparison=new ObjectComparison();//Compare object btween sourceLOD and targetLOD
 		return new LPS(linkPolicyMatcher, candidateConstructor,objectComparison);
 	}
+	//LPS(Link-Policy-Searching, using onliy Link-Policy)
 	
 	public IDSearcher getELS()
 	{
 		return new ELS();
 		
 	}
-	
+	//ELS(Explicit-Link-Searching, using only Explicit-Link)
 	public IDSearcher getCLS()
 	{
 		
 		return new CLS(getLPS(),getELS());
 	}
+	//ELS+LPS
 }
