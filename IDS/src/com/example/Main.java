90b1dce70a15752cfcf0a966e8afc27e9cf955ff
foxcats <ip9894@naver.com>
2017-04-28 02:30:27 +0900

    Update Main.java

--- IDS/src/com/example/Main.java
+++ IDS/src/com/example/Main.java
@@ -9,14 +9,17 @@ public class Main {
 	public static void main(String[] args){
 		
 	
+		
 		LinkPolicyReader linkpolicyReader=new LinkPolicyReader();
 	
 		linkpolicyReader.readLinkPolicy("ko.dbpedia.org_linkPolicy");
+		// Read Link-Policy File
 		
 		IDScontroller IDS =new IDScontroller(linkpolicyReader,"http://ko.dbpedia.org/sparql", 
 				"select * where { ?s rdfs:label ?o FILTER regex(str(?o),\"아이언맨\")}",2,0);
-		
+		//make class of IDScontroller (LinkPolicyReader,Source-SPARQL EndPoint,SPARQL Query,Depth,Similarity)
 		IDS.searchingInLOD(new IDSFactory().getCLS());
+		//Searching start(get Class from IDSFactory)
 	}
 
 }
