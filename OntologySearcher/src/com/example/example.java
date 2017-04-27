4ec2b7a7e198ca7520f60f815d449f017a45fbcd
foxcats <ip9894@naver.com>
2017-04-27 13:18:34 +0900

    Add files via upload

---
+++ OntologySearcher/src/com/example/example.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import com.os.OntologySearcher;
+
+public class example {
+
+	
+	public void main(String[] args)
+	{
+		OntologySearcher os=new OntologySearcher();
+		os.searchOntologyInfo("fileName");
+	}
+}
