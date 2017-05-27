e50d27ca45ef896b581cd335e5ad9716ebe9a689
foxcats <ip9894@naver.com>
2017-05-27 23:39:01 +0900

    Add files via upload

--- OntologySearcher/src/com/example/example.java
+++ OntologySearcher/src/com/example/example.java
@@ -7,6 +7,7 @@ public class example {
 	
 	public void main(String[] args)
 	{
+		//'LODInfo' file used for searching ontology info must be in project folder. 'LODInfo' is produced by IDS   
 		OntologySearcher os=new OntologySearcher();
 		os.searchOntologyInfo("fileName");
 	}
